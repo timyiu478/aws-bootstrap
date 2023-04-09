d6d435e4dae40a84d67c6dcf4fbe1ca5251baa4b
tim <ctom3560@gmail.com>
2023-04-09 22:58:41 +0800

    hello world

--- server.js
+++ server.js
@@ -1,24 +1,15 @@
 const { hostname } = require('os');
-const https = require('https');
-const fs = require('fs');
+const http = require('http');
 
-const STACK_NAME = process.env.STACK_NAME || "Unknown Stack";
-const httpsPort = 8443;
-const httpsKey = '../keys/key.pem'
-const httpsCert = '../keys/cert.pem'
+const message = 'Hello World\n';
+const port = 8080;
 
-if (fs.existsSync(httpsKey) && fs.existsSync(httpsCert)) {
-    console.log('Starting https server')
-    const message = `Hello HTTPS World from ${hostname()} in ${STACK_NAME}\n`;
-    const options = { key: fs.readFileSync(httpsKey), cert: fs.readFileSync(httpsCert) };
-    const server = https.createServer(options, (req, res) => {
-        res.statusCode = 200;
-        res.setHeader('Content-Type', 'text/plain');
-        res.end(message);
-    });
-    server.listen(httpsPort, hostname, () => {
-        console.log(`Server running at http://${hostname()}:${httpsPort}/`);
-    });
-} else {
-    console.log('Could not find certificate/key')
-}
+const server = http.createServer((req, res) => {
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'text/plain');
+    res.end(message);
+});
+
+server.listen(port, hostname, () => {
+    console.log(`Server running at http://${hostname()}:${port}/`);
+});
--- validate-cfn.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-CLI_PROFILE=awsbootstrap
-
-# Validate the CloudFormation template
-aws cloudformation validate-template \
-  --profile $CLI_PROFILE \
-  --template-body file://main.yml \
